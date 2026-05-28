151871cee2ebd30dfe09f33ebc3a236f2f340123
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-05-28 11:18:25 +0200

    Controlling data caching examples

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -1,17 +1,14 @@
+// import { unstable_noStore as noStore } from "next/cache"; // legacy: unstable_noStore will opt out the prerendering
+
 import Messages from "@/components/messages";
 
-export default async function MessagesPage() {
-  // Instead of using the revalidatePath(), could be part of the request too
-  const response = await fetch("http://localhost:8080/messages", {
-    next: {
-      revalidate: 5,
-    },
-  });
+// These constances will be applied to the entire file in the requests functions
+export const revalidate = 5;
+export const dynamic = "force-dynamic"; // equivalent to cache: "no-store"
 
-  // Another way to set no cache
-  // const response = await fetch("http://localhost:8080/messages", {
-  //   cache: "no-store",
-  // });
+export default async function MessagesPage() {
+  // noStore();
+  const response = await fetch("http://localhost:8080/messages");
 
   const messages = await response.json();
 
